281370e026bc9da9d84417131bf809ee5e0864a5
AuGa456 <jsl37@kent.ac.uk>
2024-10-24 10:12:43 +0000

    whileloops

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,12 @@
+import random
+number = random.randint(1,100)
+guess = 0
+while number != guess:
+    guess = int(input("Guess my random number. "))
+    if number!= guess:
+        print("Thats incorrect ")
+    else:
+        print("Your guess is correct. ")
+        break
+
 
