1a368ca720a249410166889db1fdfe43e071ba64
AuGa456 <jsl37@kent.ac.uk>
2024-10-24 13:57:18 +0000

    alltogether

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,10 @@
-### Part Four -- your code goes here. 
+import random
+ham = [random.randint(1, 100) for _ in range(10)]
+pickle = 0
+while pickle < len(ham):
+    if ham[pickle] % 2 == 0:
+        ham.pop(pickle)
+    else:
+        pickle += 1
+print(ham)
+print("Have a nice day Robbie :) ")
